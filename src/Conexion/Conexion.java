edbc5a88da31565304a5f56f141c64400574828c
Israel Huerta <israelhf24@gmail.com>
2021-05-15 16:20:27 -0500

    17 proporcionando al proyecto los archivos jar de conexion para los servidores

--- src/Conexion/Conexion.java
+++ src/Conexion/Conexion.java
@@ -6,6 +6,8 @@
 package Conexion;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
@@ -18,8 +20,18 @@ public class Conexion {
     private String password = "240395";
     private String urlMysql = "jdbc:mysql://localhost/" + db + "?SslMode=none";
     private Connection conn = null;
-    
-    public Conexion(){
-        
+
+    public Conexion() {
+        try {
+            // obtenemos el driver para mysql
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(this.urlMysql, this.user, this.password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Error : " + ex);
+        }
+    }
+
+    public Connection getConn() {
+        return conn;
     }
 }
